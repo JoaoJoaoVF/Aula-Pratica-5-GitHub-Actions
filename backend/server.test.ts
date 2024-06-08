52cc12e3af11ddcbcca0ada98140e45d1055404c
João Vitor Ferreira <jvferreira.20@gmail.com>
2024-06-08 16:12:21 -0300

    Criação de 15 testes e adaptações do servidor

---
+++ backend/server.test.ts
@@ -0,0 +1,177 @@
+
+import app from './server';
+
+const request = require('supertest');
+
+describe('Teste das rotas do servidor', () => {
+  let server: any;
+  const userData = {
+    nome: 'Teste',
+    email: 'teste@teste.com',
+    senha: '123456',
+    curso: 'Sistemas de Informação',
+    universidade: 'Universidade Federal de Minas Gerais'
+  }; 
+
+  beforeAll(() => {
+    server = app.listen();
+  });
+
+  afterAll(() => {
+    return new Promise<void>((resolve, reject) => {
+      server.close((err: Error | null) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve();
+      });
+    });
+  });
+
+  it('Deve responder com status 200 para a rota de registro', async () => {
+    const res = await request(app).post('/registro').send(userData);
+    expect(res.statusCode).toEqual(200);
+  });
+
+  it('Deve responder com status 400 para a rota de registro se o email for inválido', async () => {
+    const res = await request(app)
+      .post('/registro')
+      .send({ ...userData, email: 'email_invalido' });
+    expect(res.statusCode).toEqual(400);
+  });
+
+  it('Deve responder com status 409 para a rota de registro se o email já estiver cadastrado', async () => {
+    await request(app).post('/registro').send(userData);
+
+    const res = await request(app).post('/registro').send(userData);
+    expect(res.statusCode).toEqual(409);
+  });
+
+  it('Deve responder com status 200 para a rota de login', async () => {
+    const res = await request(app).post('/login').send(userData);
+    expect(res.statusCode).toEqual(200);
+  });
+
+  it('Deve responder com status 401 para a rota de login se as credenciais forem inválidas', async () => {
+    const res = await request(app)
+      .post('/login')
+      .send({ ...userData, senha: 'senha_incorreta' });
+    expect(res.statusCode).toEqual(400);
+  });
+
+
+  it('Deve responder com status 401 para a rota de login se o email não estiver cadastrado', async () => {
+    const res = await request(app)
+      .post('/login')
+      .send({ ...userData, email: 'email_nao_cadastrado@test.com' });
+    expect(res.statusCode).toEqual(400);
+  });
+
+  it('Deve responder com status 200 para a rota de avaliar professor', async () => {
+    const res = await request(app)
+      .post('/avaliar-professor')
+      .set('Authorization', `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJhQGEuY29tIiwiaWF0IjoxNzE3ODczMjIxLCJleHAiOjE3MTc4NzY4MjF9.7Y6hXCDjgvzPESRee0rYNpmu83VyQ6ahdzzvxPT_Js8`)
+      .send({
+        professorNome: 'Professor Teste',
+        professorMateria: 'Teste de Software',
+        professorUniversidade: 'Universidade Teste',
+        semestre: '2021/1',
+        nota: 5,
+        departamento: 'Departamento de Teste',
+        avaliacaoTexto: 'Professor muito bom'
+      });
+    expect(res.statusCode).toEqual(200);
+  });
+
+  it('Deve responder com status 400 para a rota de avaliar professor se algum campo estiver faltando', async () => {
+    const res = await request(app)
+      .post('/avaliar-professor')
+      .send({
+        professorNome: 'Professor Teste',
+        professorMateria: 'Teste de Software',
+        professorUniversidade: 'Universidade Teste',
+        semestre: '2021/1',
+        nota: 5,
+        departamento: 'Departamento de Teste'
+      });
+    expect(res.statusCode).toEqual(400);
+  });
+
+  it('Deve responder com status 400 para a rota de avaliar professor se a nota for inválida', async () => {
+    const res = await request(app)
+      .post('/avaliar-professor')
+      .set('Authorization', `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJhQGEuY29tIiwiaWF0IjoxNzE3ODczMjIxLCJleHAiOjE3MTc4NzY4MjF9.7Y6hXCDjgvzPESRee0rYNpmu83VyQ6ahdzzvxPT_Js8`)
+      .send({
+        professorNome: 'Professor Teste',
+        professorMateria: 'Teste de Software',
+        professorUniversidade: 'Universidade Teste',
+        semestre: '2021/1',
+        nota: 6,
+        departamento: 'Departamento de Teste',
+        avaliacaoTexto: 'Professor muito bom'
+      });
+    expect(res.statusCode).toEqual(400);
+  });
+
+  it('Deve responder com status 401 para a rota de avaliar professor se o token não for fornecido', async () => {
+    const res = await request(app)
+      .post('/avaliar-professor')
+      .send({
+        professorNome: 'Professor Teste',
+        professorMateria: 'Teste de Software',
+        professorUniversidade: 'Universidade Teste',
+        semestre: '2021/1',
+        nota: 5,
+        departamento: 'Departamento de Teste',
+        avaliacaoTexto: 'Professor muito bom'
+      });
+    expect(res.statusCode).toEqual(401);
+  });
+
+  it('Deve responder com status 401 para a rota de avaliar professor se o token for inválido', async () => {
+    const res = await request(app)
+      .post('/avaliar-professor')
+      .set('Authorization', 'token_invalido')
+      .send({
+        professorNome: 'Professor Teste',
+        professorMateria: 'Teste de Software',
+        professorUniversidade: 'Universidade Teste',
+        semestre: '2021/1',
+        nota: 5,
+        departamento: 'Departamento de Teste',
+        avaliacaoTexto: 'Professor muito bom'
+      });
+    expect(res.statusCode).toEqual(401);
+  });
+
+  it('Deve responder com status 200 para a rota de avaliações aprovadas', async () => {
+    const res = await request(app).get('/avaliacoes-aprovadas');
+    expect(res.statusCode).toEqual(200);
+  });
+
+  it('Deve responder com status 200 para a rota de aluno', async () => {
+    const res = await request(app)
+      .get('/aluno')
+      .set('authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJhQGEuY29tIiwiaWF0IjoxNzE3ODczMjIxLCJleHAiOjE3MTc4NzY4MjF9.7Y6hXCDjgvzPESRee0rYNpmu83VyQ6ahdzzvxPT_Js8'); 
+    expect(res.statusCode).toEqual(200);
+  });
+
+  it('Deve responder com status 401 para a rota de aluno se o token não for fornecido', async () => {
+    const res = await request(app).get('/aluno');
+    expect(res.statusCode).toEqual(401);
+  });
+
+  it('Deve responder com status 401 para a rota de aluno se o token for inválido', async () => {
+    const res = await request(app)
+      .get('/aluno')
+      .set('authorization', 'token_invalido');
+    expect(res.statusCode).toEqual(401);
+  });
+
+  it('Deve responder com status 404 para uma rota inexistente', async () => {
+    const res = await request(app).get('/rota-inexistente');
+    expect(res.statusCode).toEqual(404);
+  });
+
+});
--- backend/server.ts
+++ backend/server.ts
@@ -5,7 +5,7 @@ import bcrypt from 'bcryptjs';
 
 const port = process.env.PORT || 3000;
 const segredo = 'ohohohoh';
-const saltRounds = 12; // Número de iterações para o bcrypt
+const saltRounds = 12; 
 const cors = require('cors');
 
 const app = express();
@@ -15,16 +15,16 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Simulando um "banco de dados" com um array
 let alunos: any[] = [];
+let avaliacoes: any[] = [];
+let tiposUsuario: any[] = [];
 
-// Função para verificar se um email é válido
+
 function isValidEmail(email: string): boolean {
   const emailRegex: RegExp = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
   return emailRegex.test(email);
 }
 
-// Rota de registro
 app.post('/registro', async (req: Request, res: Response) => {
   const { nome, email, senha, curso, universidade } = req.body;
 
@@ -48,23 +48,21 @@ app.post('/registro', async (req: Request, res: Response) => {
   const novoAluno = { id: alunos.length + 1, nome, email, senha: hashedPassword, curso, universidade };
   alunos.push(novoAluno);
 
-  console.log('Aluno cadastrado com sucesso');
   res.status(200).json({ mensagem: 'Cadastro bem-sucedido' });
 });
 
-// Rota de login
 app.post('/login', async (req: Request, res: Response) => {
   const { email, senha } = req.body;
 
   const aluno = alunos.find(aluno => aluno.email === email);
   if (!aluno) {
-    res.status(401).json({ mensagem: 'Credenciais inválidas' });
+    res.status(400).json({ mensagem: 'Credenciais inválidas' });
     return;
   }
 
   const passwordMatch = await bcrypt.compare(senha, aluno.senha);
   if (!passwordMatch) {
-    res.status(401).json({ mensagem: 'Credenciais inválidas' });
+    res.status(400).json({ mensagem: 'Credenciais inválidas' });
     return;
   }
 
@@ -76,6 +74,81 @@ app.post('/login', async (req: Request, res: Response) => {
   res.status(200).json({ token });
 });
 
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+app.post('/avaliar-professor', (req: Request, res: Response) => {
+  const { professorNome, professorMateria, professorUniversidade, semestre, nota, departamento, avaliacaoTexto } = req.body;
+
+  if (!professorNome || !professorMateria || !professorUniversidade || !semestre || !nota || !departamento || !avaliacaoTexto) {
+    res.status(400).json({ mensagem: 'Todos os campos são obrigatórios' });
+    return;
+  }
+
+  const token = req.headers['authorization'];
+  if (!token) {
+    return res.status(401).json({ mensagem: 'Token não fornecido' });
+  }
+
+  jwt.verify(token, segredo, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ mensagem: 'Token inválido' });
+    }
+
+    const alunoId = (decoded as any).id;
+    if (alunoId === undefined) {
+      return res.status(401).json({ mensagem: 'Token inválido' });
+    }
+
+    const nota = Number(req.body.nota);
+    if (nota < 1 || nota > 5) {
+      return res.status(400).json({ mensagem: 'A nota deve ser um número entre 1 e 5' });
+    }
+
+    const avaliacao = {
+      alunoId,
+      professorNome,
+      professorMateria,
+      professorUniversidade,
+      semestre,
+      nota,
+      departamento,
+      avaliacaoTexto,
+      aprovada: false
+    };
+    avaliacoes.push(avaliacao);
+
+    console.log('Avaliação do professor cadastrada com sucesso e aguardando aprovação');
+    res.status(200).json({ mensagem: 'Avaliação cadastrada com sucesso e aguardando aprovação' });
+  });
+
 });
+
+app.get('/avaliacoes-aprovadas', (req: Request, res: Response) => {
+  res.status(200).json(avaliacoes);
+});
+
+
+app.get('/aluno', (req: Request, res: Response) => {
+  const token = req.headers['authorization'];
+  if (!token) {
+    return res.status(401).json({ mensagem: 'Token não fornecido' });
+  }
+
+  jwt.verify(token, segredo, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ mensagem: 'Token inválido' });
+    }
+
+    const alunoId = (decoded as any).id;
+    if (alunoId === undefined) {
+      return res.status(401).json({ mensagem: 'Token inválido' });
+    }
+    console.log(" :) ", alunoId);
+    const aluno = alunos.find(aluno => aluno.id === alunoId);
+    if (!aluno) {
+      return res.status(404).json({ mensagem: 'Aluno não encontrado' });
+    } else {
+      res.status(200).json(aluno);
+    }
+  });
+});
+
+export default app;
